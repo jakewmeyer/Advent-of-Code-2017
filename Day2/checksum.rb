cc30f5f4ab2e5a4ba9dd3f36298bcd1671dd9988
Jake Meyer <jakewmeyer@gmail.com>
2017-12-03 06:05:22 -0600

    added day 2

---
+++ Day2/checksum.rb
@@ -0,0 +1,31 @@
+
+require "csv"
+
+def checksum1
+    sum = 0
+    convert = lambda { |field, _| field.strip.to_i }
+
+    CSV.foreach("input.txt", converters: convert, :col_sep => "\t") do |row|
+        value = (row.max) - (row.min)
+        sum += value
+    end
+    puts sum
+end
+
+def checksum2
+    sum = 0
+    convert = lambda { |field, _| field.strip.to_i }
+
+    CSV.foreach("input.txt", converters: convert, :col_sep => "\t") do |row|
+        combos = row.combination(2).to_a
+        combos.each do |i|
+            if i.max % i.min == 0
+                sum += (i.max / i.min)
+            end
+        end
+    end
+    puts sum
+end
+
+checksum1
+checksum2
