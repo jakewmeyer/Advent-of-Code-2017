838bab489987870cc7e037b137d9b7328573a5f3
Jake Meyer <jakewmeyer@gmail.com>
2017-12-05 09:09:12 -0600

    Day 4 rubocop fixes

--- Day4/passphrase.rb
+++ Day4/passphrase.rb
@@ -1,28 +1,24 @@
 
-require "csv"
+require 'csv'
 
 def passphrase1
-    valid = 0
-    CSV.foreach("input.txt", :col_sep => "\s") do |row|
-        if row == row.uniq
-            valid += 1
-        end
-    end
-    puts valid
+  valid = 0
+  CSV.foreach('input.txt', col_sep: "\s") do |row|
+    valid += 1 if row == row.uniq
+  end
+  puts valid
 end
 
 def passphrase2
-    count = 0
-    CSV.foreach("input.txt", :col_sep => "\s") do |row|
-        unique = []
-        row.each do |word|
-            unique.push(word.chars.sort.join)
-        end
-        if unique == unique.uniq
-            count += 1
-        end
-    end
-    puts count
+  count = 0
+  CSV.foreach('input.txt', col_sep: "\s") do |row|
+    unique = []
+    row.each do |word|
+      unique.push(word.chars.sort.join)
+    end 
+    count += 1 if unique == unique.uniq
+  end
+  puts count
 end
 
 passphrase1
